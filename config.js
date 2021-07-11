efd04ba5f801a2c711bde740725af861efa30c61
Selmane-ishak <73941369+Selmane-ishak@users.noreply.github.com>
2021-07-11 16:52:14 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,65 @@
+/* configuration default view*/
+var init = {
+    coordinate: [29.56282445, 3.12480569],
+    zoom: 5,
+    basemap:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    attribution: 'Â© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+}
+
+/* configuration of slides*/
+var storysSliderContent = [
+    {
+        id: "1",
+        title: '"Lac Dhaia" à Tamesguida',
+        imageUrl: "https://www.aps.dz/media/k2/items/cache/fd0b6f05bcaced5d2cc7f6990b9a59dc_M.jpg",
+        description: "Le lac Dhaya constitue une destination de choix pour les amoureux du calme et de la détente, au vu de sa situation géographique sur les monts Tamezguida (Médéa), aux frontières administratives avec la wilaya de Blida. Chaque fin de semaine, ce lieu paradisiaque, situé à 1.230 m d'altitude, est le point de convergence de nombreux visiteurs de différentes wilayas du pays. Ce lieu a été sélectionné par la Fédération algérienne de sport pour tous, en coordination avec la direction de la Jeunesse et des sports de Blida, pour faire l'objet samedi d'une virée touristique, à l'occasion de la célébration de la Journée nationale du savoir, coïncidant avec le 16 avril de chaque année. Le lac Dhaya fait partie des lieux touristiques les plus prisés par les établissements éducatifs et juvéniles de nombreuses  wilayas du pays, qui le privilégient pour abriter de nombreuses activités de détente, culturelles et environnementales.",
+        coordinate: [36.3666256282445, 2.6918011248056954],
+        zoom: 17,
+    },
+
+    {
+        id: "2",
+        title: "Moul N'aga - Un petit lac dans le désert",
+        imageUrl: "https://media.routard.com/image/13/1/photo.1553131.jpg",
+        description: "Un lieu légendaire dont vous ne pouvez pas croire l'existence à moins de le voir. Des morceaux de roche sculptés par la nature dans des formes distinctes et étranges dans un endroit où le sable et les rochers se chevauchent pour créer de belles peintures de la vaste terre d'Algérie",
+        coordinate: [24.003686313463046, 10.944670402320641],
+        zoom: 11,
+    },
+
+    {
+        id: "3",
+        title: "Lac de Réghaïa",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/1/10/Photo_la_reghaia_29052016.jpg",
+        description: "Le lac de Réghaïa est un lac situé dans le territoire de la commune de Réghaïa distante de 29 kilomètres à l'est de la ville d'Alger.Il recueille les eaux d'un bassin versant de 842 km2 d'où débouche l'Oued Réghaïa. Le lac avec la forêt de Réghaïa, a été reconnu site Ramsar le 4 juin 2003.",
+        coordinate: [36.771650943898166, 3.334972813021878],
+        zoom: 14,
+    },
+
+    {
+        id: "4",
+        title: "Lac Noir (Akfadou) 'Agelmim Aberkan'",
+        imageUrl: "https://pbs.twimg.com/media/C73j1QgW4AAtomp.jpg",
+        description: "Le lac Noir est un lac naturel, situé dans la forêt de l'Akfadou dans la commune d'Adekar à 1 260 m d'altitude, est d'une superficie d'environ 3 hectares et d'un mètre de profondeur. Une digue artificielle a été construite d'un côté du lac pour maintenir un niveau d'eau appréciable.",
+        coordinate: [36.69630584082945, 4.602556645978922],
+        zoom: 16,
+    },
+
+    {
+        id: "5",
+        title: "Lac Tonga",
+        imageUrl: "https://live.staticflickr.com/4056/5163659065_4fdae780c8_z.jpg",
+        description: "Le site est un vaste complexe de zones humides qui comprend un lac d’eau douce saisonnier, des collines boisées, une forêt humide et un système de dunes de sable, tous situés dans le Parc national El Kala, à l’extrême nord‑est du pays. Le lac est relié à la Méditerranée et offre un habitat important aux plantes aquatiques. C’est le site de nidification le plus important d’Afrique du Nord pour une multitude d’espèces d’oiseaux, notamment des colonies de hérons et d’aigrettes, le busard des roseaux et des oiseaux d’eau variés, y compris des espèces rares et menacées au plan mondial. Les communautés locales pratiquent l’élevage et une agriculture saisonnière. Les questions de gestion comprennent la réglementation de la pêche et l’utilisation de l’eau du lac pour l’irrigation ainsi que la restauration des fonctions hydrologiques naturelles du lac. Le site a été inscrit au Registre de Montreux en 1993 parce que le lac était couvert de plantes aquatiques émergentes, laissant peu d’espace aux eaux ouvertes. Une réduction importante de l’alimentation en eau du lac est à l’origine de changements dans les caractéristiques écologiques.",
+        coordinate: [36.85825574265865, 8.498534573514478],
+        zoom: 13,
+    },
+
+
+    
+
+
+
+
+
+    
+
+];
---
+++ dist/main.js
@@ -0,0 +1,80 @@
+function Storys(
+    id,
+    title,
+    imageUrl,
+    description
+) {
+    var sliders = `
+<div class="slide selected selected_slide" style="height: 336px;">
+    <h1>${title}</h1>
+    <p><img alt="New York" src=${imageUrl}></p>
+    <p>${description}</p>
+</div>    
+`;
+    return sliders;
+}
+
+
+
+
+var map = L.map('map')
+    .setView(init.coordinate, init.zoom);
+var osm = new L.tileLayer(init.basemap, {
+    attribution: init.attribution
+}).addTo(map);
+
+/* console.log(storysSliderContent.length); */
+
+var displayer = document.getElementById('displayer');
+var PageNumber = document.getElementById('number');
+dsiplayer.innerHTML =`
+<div class="slide selected selected_slide" style="height: 336px;">
+    <h1>Commencer le diaporama</h1>
+    <p><img alt="" src="dist/img/diaporama.png" style="height: 260px;width:260px;"></p>
+    <p>Cliquez sur les deux flêche afin de commencer le diaporama.</p>
+</div>    
+`;
+PageNumber.textContent = "Diapo N° : "+(0)+'/'+storysSliderContent.length;
+/* dsiplayedSlide(0); */
+
+var $i = -1;
+
+function next() {
+    $i = $i >= storysSliderContent.length - 1 ? 0 : $i + 1;
+    //diplayer.textContent = geojson.features[i].properties.address;
+    dsiplayedSlide($i);
+    /* console.log($i) */
+}
+
+function prev() {
+    $i = $i > 0 ? $i - 1 : storysSliderContent.length - 1;
+    //diplayer.textContent = geojson.features[i].properties.address;
+    dsiplayedSlide($i);
+}
+
+$(document).ready(function () {
+
+    $('#next').on('click', next)
+
+    $('#prev').on('click', prev)
+
+});
+
+function dsiplayedSlide(e) {
+
+    map.setView(storysSliderContent[e].coordinate, storysSliderContent[e].zoom);
+
+    console.log(storysSliderContent[e].title);
+
+    dsiplayer.innerHTML = Storys(
+        storysSliderContent[e].id,
+        storysSliderContent[e].title,
+        storysSliderContent[e].imageUrl,
+        storysSliderContent[e].description
+
+    );
+
+    console.log((e+1)+'/'+storysSliderContent.length);
+    PageNumber.textContent = "Diapo N° : "+(e+1)+'/'+storysSliderContent.length;
+
+}
